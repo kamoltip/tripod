192f45c6417998a96f42ee2998e3d2a52ddc564b
kamoltip <plawansusu@gmail.com>
2017-10-07 06:49:38 -0400

    feature page fix

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,48 +1,50 @@
-import React, { Component } from 'react'
-import { Icon, Menu } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import {Icon, Menu} from 'semantic-ui-react'
 import './Menu.css';
 import crew from '../../asset/images/user.png'
+import logo from '../../asset/images/tripod-logo.png'
 
 export default class MenuExampleLabeledIcons extends Component {
-  state = { activeItem: 'gamepad' }
+  state = {
+    activeItem: 'gamepad'
+  }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, {name}) => this.setState({activeItem: name})
 
   render() {
-    const { activeItem } = this.state
+    const {activeItem} = this.state
 
     return (
-      <Menu pointing size='tiny'>
+      <Menu pointing inverted size='tiny'>
+        <Menu.Item header><img src={logo}/></Menu.Item>
         <Menu.Item name='camera' active={activeItem === 'camera'} onClick={this.handleItemClick}>
-          <img src='http://www.stickpng.com/assets/images/584abe602912007028bd932f.png' alt='camera' />
+        <Icon name='camera' size='big'/>
           Take a Shot
         </Menu.Item>
 
         <Menu.Item name='picFolder' active={activeItem === 'picFolder'} onClick={this.handleItemClick}>
-          <img src='http://www.workingatmcmaster.ca/med/photo/large/gallery-icon-1-627.png' alt='picFolder' />
+          <Icon name='image' size='big'/>
           Images
         </Menu.Item>
 
         <Menu.Item name='search' active={activeItem === 'search'} onClick={this.handleItemClick}>
-          <img src='https://maxcdn.icons8.com/Share/icon/Security//facial_recognition_scan1600.png' alt='scanFace' />
+          <Icon name='search' size='big'/>
           Search
         </Menu.Item>
         <Menu.Item name='feature' active={activeItem === 'feature'} onClick={this.handleItemClick}>
-          < Icon name='info circle' size='big'/>
+          <Icon name='info circle' size='big'/>
           Feature
         </Menu.Item>
         <Menu.Menu position='right'>
           <Menu.Item name='crew' active={activeItem === 'crew'} onClick={this.handleItemClick}>
-            <img src={crew} alt='crew' />
-            Crew
+            <img src={crew} alt='crew'/>
+            
           </Menu.Item>
           <Menu.Item name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick}>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSP4CLMcDvMhEGhbG_a9f0gwmdFvO3oXMK9JntkLPNoVyP_SSs' alt='home' />
-            Home
+            <Icon name='home' size='big'/>
           </Menu.Item>
           <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick}>
-            <img src='https://www.shareicon.net/download/2015/10/17/657490_arrow_512x512.png' alt='logout' />
-            Logout
+            <Icon name='sign out' size='big'/>
           </Menu.Item>
         </Menu.Menu>
       </Menu>
