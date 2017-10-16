d16b9f8f8f1ebe1ee0b68fb117051cd4bcee5945
Dil <dnks2000@gmail.com>
2017-10-16 16:57:46 -0400

    some feachers search

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -3,36 +3,64 @@ import React , { Component } from 'react';
 import { render } from 'react-dom';
 import './Search.scss';
 import {Link} from 'react-router';
-import {Segment, Grid, Image, Divider, Responsive, Button} from 'semantic-ui-react';
+import {Segment, Grid, Divider, Image, Responsive, Button, Container, Checkbox, Popup} from 'semantic-ui-react';
 import tripod from '../../asset/images/tripod-logo.png';
 import API from '../../utils/API';
+import { CloudinaryContext, Transformation } from 'cloudinary-react';
 
 
 // have to add - click to enlarge the img/ download buttons/ rotations / etc
-
 
 class Search extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      images: [],
-      url: "",
-      date: "",
-      pid: ""
+      gallery: [],
+      checkedImg: [],
+      selectedImage: '',
+      width_large: true
     };
 
     this.searchImages = this.searchImages.bind(this);
+    // this.enlargeImg = this.enlargeImg.bind(this);
   };
+
+  componentDidMount() {
+    // window.addEventListener('load', this.searchImages); // h
+  }
 
   searchImages(event) {
     event.preventDefault();
       API.getPicDetails()
-      .then(res => this.setState({ images: res.data})) ////console.log(res.data)) 
+      .then(res => this.setState({ gallery: res.data}))
       .catch(err => console.log(err));
   };
-  
+
+  onChangeDownload(checked){
+    console.log(checked);
+    this.state.checkedImg.push(checked);
+    console.log(this.state.checkedImg);
+  };
+
+  changeColor(key,url){
+    alert(url);
+    // this.setState({width_large: !this.state.width_large})
+
+    <Popup trigger={<Image key={this.key} />}  style={style}>
+      Hello. This is a regular pop-up which does not allow for lots
+      of content. You cannot fit a lot of words here as the
+      paragraphs will be pretty narrow.
+      <Image src={this.url} />
+    </Popup>
+  }
+
+  download(event){
+    //save checkedImg array
+  };
+
   render() {
+
     return (
       <div>
         <Responsive {...Responsive.onlyMobile} {...Responsive.onlyTablet} {...Responsive.onlyComputer} {...Responsive.onlyLargeScreen} {...Responsive.onlyWidescreen} as={Grid}/>
@@ -40,27 +68,52 @@ class Search extends Component {
         <div className='searchDiv'>
           <div className='metaDiv'>
             <Link to='/activity'><img className='logo' src={tripod}/></Link>
-            <Divider section/>
+            {/* <Divider section/> */}
             <div className='thirdDiv'>
-              <p className='text'>Find your photos
-                <br/>
-              collection</p>
               <div className='fourthDiv'>
-                 
-                  {this.state.images.map(image => (
-                    <panel key={ image._id }>
-                      <a href={ image.pic_url } target='_blank'><img style={style.displayImage} src={ image.pic_url } /></a>
-                    </panel>
-                  ))}
-                <div className='fifthDiv'>
-                  <Button fluid size='large' color='green' content='Download'/>
-                  <Button fluid size='large' color='red' onClick={this.searchImages} content='Search' />
-                  <Button fluid size='large' color='green' content='Sort Date'/>
-                </div>
+                <div className='searchText'><h1>FIND YOUR PHOTO'S COLLECTION</h1></div>
+
+
+                  <CloudinaryContext cloudName="tripod"> 
+                    {this.state.gallery.map(data => (
+                      <div className="responsive" key={data._id} style={style.responsive} onClick={ () => this.changeColor(data._id,data.pic_url)} >
+                        <div className="img" style={style.img} >
+                            <Image key={data.pic_url} style={style.img}>
+                              <img src ={data.pic_url} />
+                              <Transformation
+                                crop="scale"
+                                width="100px"
+                                height="100px"
+                                dpr="auto"
+                                responsive_placeholder="blank"
+                              />
+                              <input type="checkbox" 
+                                     name={data.pic_public_id} 
+                                     key={data._id}
+                                     onChange={ () => this.onChangeDownload(data)} 
+                                     defaultChecked={false} /> {data.pic_date}
+                            </Image> 
+                        </div>
+                      </div>
+                    ))}
+                  </CloudinaryContext> 
+
+                    <Divider section/>
+                  <Container>
+                  <Button.Group>
+                  <Button size='big' floated='left' color='red' onClick={this.searchImages} content='Search Photos' />
+                  <Button size='big' floated='right' color='green' content='Download' onClick={this.download} />
+       
+                </Button.Group>
+                </Container>
               </div>
             </div>
           </div>
+          <div className='fifthDiv'>
+
+          </div>
         </div>
+
       </div>
 
     );
@@ -69,12 +122,30 @@ class Search extends Component {
 };
 
 const style = {
-  displayImage: {
-    width: '10%',
-    height: 'auto',
-    padding:'1%'
+  // displayImage: {
+  //   width: '20%',
+  //   height: 'auto',
+  //   padding:'2%',
+  //   border:'2px solid white',
+  //   display:'inline-grid',
+  //   marginTop:'1%',
+  //   marginRight:'1%',
+  //   marginLeft:'1%',
+  //   marginBottom:'1%',
+  // },
+  img:{
+    border: '1px solid #ccc'
+  },
+  responsive: {
+    padding:'0 6px',
+    float:'left',
+    width:'24%',
+    margin:'10px'
+  },
+  desc: {
+    padding: '15px',
+    textAlign:'center'
   }
+
 };
 export default Search;
-
-
