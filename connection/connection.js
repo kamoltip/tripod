ef94e4ada05485e2e89b5681e69384c16c4ef401
jliip51 <jliip51@gmail.com>
2017-10-19 07:36:23 -0400

    fix connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(
 // process.env.MONGODB_URI || 'mongodb://admin:tripod@ds117605.mlab.com:17605/heroku_r1s063tw',
   process.env.MONGODB_URI || 'mongodb://localhost/tripod',
-  { useMongoClient: true },
+  { useMongoClient: true }
 );
 
 const db = mongoose.connection;
